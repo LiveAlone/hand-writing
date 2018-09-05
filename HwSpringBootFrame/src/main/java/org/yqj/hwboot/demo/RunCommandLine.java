2286ab01d8b787b8abe56e7658a38d271fd41509
qijunyao <qijunyao@xiaohongshu.com>
2018-09-05 12:56:57 +0800

    re name spring configuration

---
+++ HwSpringBootFrame/src/main/java/org/yqj/hwboot/demo/RunCommandLine.java
@@ -0,0 +1,25 @@
+package org.yqj.hwboot.demo;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2018/8/15
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Component
+@Slf4j
+public class RunCommandLine implements CommandLineRunner{
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(" command info run");
+        log.warn("now command line run ");
+        log.warn("args content is " + Lists.newArrayList(args).toString());
+    }
+}
